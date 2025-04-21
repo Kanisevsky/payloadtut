4065da7cd56b4d3b5357193dc58bf88bb45a81fe
Kanisevsky <meltsmokestudio@gmail.com>
2025-04-21 22:54:17 +0100

    02: Customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,29 @@
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
+import { Progress } from '@/components/ui/progress';
+import { Textarea } from '@/components/ui/textarea';
+
 export default function Home() {
-  return <div className="text-rose-500 font-bold">BUN</div>;
+  return (
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div className="">
+          <Button variant="elevated">I am a button</Button>
+        </div>
+        <div className="">
+          <Input placeholder="I am input" />
+        </div>
+        <div className="">
+          <Progress value={50} />
+        </div>
+        <div className="">
+          <Textarea placeholder="I am text Area" />
+        </div>
+        <div className="">
+          <Checkbox />
+        </div>
+      </div>
+    </div>
+  );
 }
